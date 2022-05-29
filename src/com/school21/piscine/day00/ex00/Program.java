554009c6ddcedec640f9a9ba2a5bd59b54488b85
GuDimz <zgudimz@gmail.com>
2022-05-29 21:56:25 +0300

    day00 ex00 delete while

--- src/com/school21/piscine/day00/ex00/Program.java
+++ src/com/school21/piscine/day00/ex00/Program.java
@@ -3,14 +3,20 @@ package com.school21.piscine.day00.ex00;
 public class Program {
     public static void main(String[] args) {
         int digit = 479598;
-        if (digit < 0) {
-            digit *= -1;
-        }
         int result = 0;
-        while (digit > 0) {
-            result += digit % 10;
-            digit /= 10;
-        }
+
+        result += digit % 10;
+        digit /= 10;
+        result += digit % 10;
+        digit /= 10;
+        result += digit % 10;
+        digit /= 10;
+        result += digit % 10;
+        digit /= 10;
+        result += digit % 10;
+        digit /= 10;
+        result += digit % 10;
+
         System.out.println(result);
     }
 }
